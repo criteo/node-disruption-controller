215e8fd5bb12c4f04725ea740add39c2260f17c6
Jeremy Clerc <j.clerc@criteo.com>
2026-06-09 22:17:13 +0200

    Add support to write logs to a file
    This will allow a sidecar to process the logs, either
    for indexing or any other usecase.

--- cmd/main.go
+++ cmd/main.go
@@ -19,14 +19,18 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 	"time"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
+
+	"go.uber.org/zap/zapcore"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
+	"gopkg.in/natefinch/lumberjack.v2"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -63,6 +67,8 @@ func main() {
 	var healthHookTimeout time.Duration
 	var nodeDisruptionTypesRaw string
 	var defaultNodeDisruptionTypesRaw string
+	var logConsole bool
+	var logPath string
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
@@ -74,6 +80,8 @@ func main() {
 	flag.DurationVar(&healthHookTimeout, "healthhook-timeout", controller.DefaultHealthHookTimeout, "HTTP client timeout for calling HealthHook resolved from ADB")
 	flag.StringVar(&nodeDisruptionTypesRaw, "node-disruption-types", "", "The list of types allowed for a node disruption separated by a comma.")
 	flag.StringVar(&defaultNodeDisruptionTypesRaw, "default-node-disruption-types", "", "The default list of node disruption types for ADBs that don't specify supportedNodeDisruptionTypes. Must be a subset of --node-disruption-types.")
+	flag.BoolVar(&logConsole, "log-console", true, "Write logs to the console (stderr)")
+	flag.StringVar(&logPath, "log-path", "", "Write logs to file at path")
 
 	opts := zap.Options{
 		Development: true,
@@ -81,6 +89,7 @@ func main() {
 	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
 
+	opts.DestWriter = logOutput(logPath, logConsole)
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
@@ -173,4 +182,23 @@ func main() {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
+}
+
+func logOutput(logPath string, logConsole bool) io.Writer {
+	if logPath == "" {
+		return nil // When not set, defaults to console (stderr) anyway.
+	}
+
+	w := &lumberjack.Logger{
+		Filename:   logPath,
+		MaxSize:    10,
+		MaxBackups: 1,
+		Compress:   false, // disabled by default
+	}
+	if logConsole {
+		return zapcore.NewMultiWriteSyncer(
+			zapcore.AddSync(w),
+			zapcore.AddSync(os.Stderr))
+	}
+	return w
 }
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/onsi/ginkgo/v2 v2.22.0
 	github.com/onsi/gomega v1.36.1
 	github.com/stretchr/testify v1.11.1
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 	k8s.io/api v0.34.2
 	k8s.io/apimachinery v0.34.2
 	k8s.io/client-go v0.34.2
